b83d251c2fb66ed3519f5056bd6da39ed8c8f721
takaoh717 <hrgc.tko0717@gmail.com>
2019-03-21 17:09:06 +0900

    Initial Commit

---
+++ TestLabSampleTests/TestLabSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestLabSampleTests.swift
+//  TestLabSampleTests
+//
+//  Created by Takao Horiguchi on 2019/03/21.
+//  Copyright © 2019 takaoh717. All rights reserved.
+//
+
+import XCTest
+@testable import TestLabSample
+
+class TestLabSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
